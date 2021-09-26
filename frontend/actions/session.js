c6d7bbed73c0faeab3d9920f4e518d3ada9f3ccb
Danny Feng <dannyfeng1@aol.com>
2021-09-25 21:33:54 -0700

    finished user/session actions and api_util

---
+++ frontend/actions/session.js
@@ -0,0 +1,44 @@
+import * as UsersAPI from "../util/user_api_util";
+
+export const CREATE_USER = "CREATE_USER";
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+})
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+})
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+})
+
+export const signup = user => dispatch => {
+  return UsersAPI.signup(user)
+    .then(
+      user => dispatch(receiveCurrentUser(user)),
+      error => dispatch(receiveErrors(error))
+    )
+}
+
+export const login = user => dispatch => {
+  return UsersAPI.login(user)
+    .then(
+      user => dispatch(receiveCurrentUser(user)),
+      error => dispatch(receiveErrors(error))
+    )
+}
+
+export const logout = () => dispatch => {
+  return UsersAPI.logout()
+    .then(
+      () => dispatch(logoutCurrentUser()),
+      error => dispatch(receiveErrors(error))
+    )
+}
