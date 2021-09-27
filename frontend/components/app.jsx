074c80497e5b2919306f25f45898ff6c7ceea8f4
Danny Feng <dannyfeng1@aol.com>
2021-09-27 14:59:24 -0700

    fixed 404 error page routing

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import LogInSignUpContainer from '../components/user_auth/login_signup_container'
 import LoginFormContainer from '../components/user_auth/login_form_container';
@@ -7,16 +7,21 @@ import SignUpFormContainer from '../components/user_auth/signup_form_container';
 import DemoContainer from '../components/user_auth/demo_container'
 import { AuthRoute, ProtectedRoute } from "../util/route_utils";
 import Dashboard from './dashboard/dashboard';
+import RoutingError from './routing_error'
 
 const App = () => (
   <div>
     <NavBarContainer />
-    <Route exact path="/" component={LogInSignUpContainer}/>
-    <AuthRoute path="/signup" component={SignUpFormContainer} />
-    <AuthRoute path="/login" component={LoginFormContainer} />
-    <AuthRoute path="/demo" component={DemoContainer} />
-    <ProtectedRoute path="/dashboard" component={Dashboard}/>
-    {/* <Route exact path="/" component={SearchContainer} /> */}
+
+    <Switch>
+      <ProtectedRoute path="/dashboard" component={Dashboard}/>
+      <AuthRoute path="/signup" component={SignUpFormContainer} />
+      <AuthRoute path="/login" component={LoginFormContainer} />
+      <AuthRoute path="/demo" component={DemoContainer} />
+      <Route exact path="/" component={LogInSignUpContainer}/>
+      <Route component={RoutingError} />
+    </Switch>
+
   </div>
 )
 
