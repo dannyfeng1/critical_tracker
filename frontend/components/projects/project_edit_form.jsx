3a76ad97c36dce23d5aee1bb16a61c1f51a13376
Danny Feng <dannyfeng1@aol.com>
2021-10-06 21:19:05 -0700

    finished project edit form

--- frontend/components/projects/project_edit_form.jsx
+++ frontend/components/projects/project_edit_form.jsx
@@ -12,14 +12,17 @@ const mapStateToProps = (state, ownProps) => ({
 const mapDispatchToProps = dispatch => ({
   getProject: (projectId) => dispatch(getProject(projectId)),
   deleteProject: (projectId) => dispatch(deleteProject(projectId)),
-  getProject: (projectId) => dispatch(getProject(projectId))
+  updateProject: (project) => dispatch(updateProject(project))
 })
 
 class EditProjectForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      deleteProject: false
+    };
     this.handleDelete = this.handleDelete.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -33,10 +36,30 @@ class EditProjectForm extends React.Component {
     this.props.history.push("/dashboard")
   }
 
+  update(field) {
+    return e => this.setState({[field]: e.target.value})
+  }
+
+  handleSubmit() {
+    this.props.updateProject(this.state)
+    .then(() => this.props.history.push(`/projects/${this.props.projectId}/information`))
+  }
+
   render() {
     let { title, description } = this.state;
     if (!this.props.project) return null;
-    
+
+    let deleteConfirmation = null;
+    if (this.state.deleteProject) {
+      deleteConfirmation = (
+        <div id="delete-confirm">
+          <p>Are you sure you want to delete this project?</p>
+          <button onClick={this.handleDelete}>Yes</button>
+          <button onClick={() => this.setState({deleteProject: false})}>No</button>
+        </div>
+      )
+    }
+
     return (
       <div id="edit-project-form">
         <div className="project-header">
@@ -45,7 +68,18 @@ class EditProjectForm extends React.Component {
             <button onClick={() => this.props.history.push(`/projects/${project.id}/members`)}>Members</button>
             <button onClick={() => this.props.history.push(`/projects/${project.id}/information`)}>Information</button>
         </div>
-        <div>{title + description}</div>
+        <form>
+          <label>Title:
+            <input type="text" value={title} onChange={this.update("title")}/>
+          </label>
+          <label>Description:
+            <textarea value={description} onChange={this.update("description")} cols="30" rows="10"></textarea>
+          </label>
+          <button onClick={this.handleSubmit}>Save and Update</button>
+          <button onClick={() => this.props.history.push(`/projects/${this.props.projectId}/information`)}>Cancel</button>
+          <button onClick={() => this.setState({deleteProject: true})}>Delete</button>
+          {deleteConfirmation}
+        </form>
       </div>
     )
   }
